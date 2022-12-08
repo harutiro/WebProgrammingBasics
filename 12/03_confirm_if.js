498faeade7ac698076fb6436c5d387211e79f593
harutiro <harutiro2027@outlook.jp>
2022-12-08 10:53:04 +0900

    update: プログラム完成

---
+++ 12/03_confirm_if.js
@@ -0,0 +1,6 @@
+const result = confirm("本当に消去してよいですか？");
+if(result){
+    console.log("削除しました");
+}else{
+    console.log("キャンセルしました");
+}
---
+++ 12/06_set_interval.js
@@ -0,0 +1,5 @@
+function delayTask(){
+    console.log("1秒ごとに繰り返し実行される。");
+}
+
+setInterval(delayTask,1000);
